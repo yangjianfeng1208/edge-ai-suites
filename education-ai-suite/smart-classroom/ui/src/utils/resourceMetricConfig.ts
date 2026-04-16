94f57dc2740f991df347b779ec434dc129c452e9
Xiaojing Liu <xiaojing.liu@intel.com>
2026-04-16 17:55:00 +0800

    Add content search model to UI
    Signed-off-by: Xiaojing Liu <xiaojing.liu@intel.com>

--- education-ai-suite/smart-classroom/ui/src/utils/resourceMetricConfig.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Maximum duration for resource metric polling.
- * After this duration, polling is automatically paused and the user is prompted to resume.
- * Default: 45 minutes (in milliseconds)
- */
-export const RESOURCE_METRIC_DURATION_MIN = 45;
-export const RESOURCE_METRIC_DURATION_MS = RESOURCE_METRIC_DURATION_MIN * 60 * 1000;
